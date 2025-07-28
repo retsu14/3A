54995659d377a5ef2a1b9cf3618ac548f3dba568
retsu14 <tajanlangitelieser19@gmail.com>
2025-07-28 19:52:52 +0800

    chore: post requests

--- server.js
+++ server.js
@@ -4,8 +4,22 @@ const PORT = 5000;
 
 const profile = require("./profiles.json");
 
+// // nested objecgt
+// const req = {
+//   body: [
+//     {
+//       name: "test",
+//       email: "test",
+//     },
+//   ],
+// };
+
+// const { name, email } = req.body[0];
+
+// console.log("name", name);
+
 // middleware
-// app.use(express.json());
+app.use(express.json());
 
 // HTTP REQUESTS
 // GET
@@ -19,6 +33,17 @@ const profile = require("./profiles.json");
 
 // sampleFunction("John Doe", 12);
 
+// const body = {
+//   name: "test",
+//   email: "test@gmail.com",
+// };
+
+// // destructure
+// const { name, email } = body;
+
+// console.log(name);
+// console.log(email);
+
 // app.post("/api/like", function (req, res) {
 //   res.json({
 //     message: "Liked",
@@ -27,6 +52,18 @@ const profile = require("./profiles.json");
 
 app.get("/api/get-profiles", (req, res) => {
   res.json(profile);
+});
+
+app.post("/api/profile", function (req, res) {
+  const { name, email } = req.body;
+
+  const newUser = { name, email };
+  const data = profile.push(newUser);
+
+  res.json({
+    message: "Success",
+    data,
+  });
 });
 
 // API Endpoint
